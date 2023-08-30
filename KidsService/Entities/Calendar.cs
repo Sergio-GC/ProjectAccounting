ab5ada32505c139cd17bfd2e40b99a5e322256a0
Sergio <gomez.c.sergio@outlook.com>
2023-08-30 18:09:26 +0200

    Create Entities

---
+++ KidsService/Entities/Calendar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsService
+{
+    public class Calendar
+    {
+        [Key]
+        public int Id { get; set; }
+        public Kid Kid { get; set; }
+        public DateTime Date { get; set; }
+        public string Hours { get; set; }
+        public double Price { get; set; }
+        public Boolean IsDefinitive { get; set; }
+    }
+}
---
+++ KidsService/Entities/Kid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsService
+{
+    public class Kid
+    {
+        [Key]
+        public int Id {get; set;}
+        public string Name { get; set;}
+        public string LastName { get; set;}
+        public DateTime Birthdate { get; set;}
+        public List<Kid> Siblings { get; set;}
+    }
+}
