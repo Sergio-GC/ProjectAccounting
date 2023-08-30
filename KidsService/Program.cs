339aa9b75e889dc1888a3a135bb68e957db8f5a6
Sergio <gomez.c.sergio@outlook.com>
2023-08-30 17:48:56 +0200

    First commit - Default code

---
+++ KidsService/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KidsService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
