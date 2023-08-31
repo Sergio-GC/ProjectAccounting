2ab494b50cc97d4e34e88c0c6d30d5a69920d7d1
Sergio <gomez.c.sergio@outlook.com>
2023-08-31 13:55:17 +0200

    Connect to database

---
+++ KidsService/KidsContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace KidsService
+{
+    public class KidsContext : DbContext
+    {
+        public DbSet<Kid> Kids { get; set; }
+        public DbSet<Calendar> Calendars { get; set; }
+
+        MySqlConnectionStringBuilder myBuilder = new MySqlConnectionStringBuilder()
+        {
+            Database = "pkids",
+            UserID = "admin",
+            Password = "rootadmin",
+            Server = "192.168.0.200",
+            Port = 3306,
+        };
+
+        public KidsContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseLazyLoadingProxies();
+            builder.UseMySQL(myBuilder.ConnectionString);
+        }
+    }
+}
--- KidsService/Program.cs
+++ KidsService/Program.cs
@@ -2,13 +2,44 @@
 {
     internal class Program
     {
+        static KidsContext ctx;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Kid kid = new Kid();
-            kid.Name = "Test kiddo";
+            ctx = new KidsContext();
+            bool e = ctx.Database.EnsureCreated();
 
-            Console.WriteLine(kid.Name);
+            if(e)
+            {
+                Console.WriteLine("Database has been created!");
+
+                initDB();
+                Console.WriteLine("Done!");
+            }
+            else
+            {
+                Console.WriteLine("Database already exists :3");
+            }
+        }
+
+        private static void initDB()
+        {
+            Console.WriteLine("Inserting demo data...");
+
+            ctx.Kids.Add(new Kid()
+            {
+                Name = "Gabriel",
+                LastName = "Dos Santos",
+                Birthdate = new DateTime(2021, 10, 9)
+            });
+
+            ctx.Kids.Add(new Kid()
+            {
+                Name = "Carolina",
+                LastName = "Do Pico",
+                Birthdate = new DateTime(2018, 03, 23)
+            });
+
+            ctx.SaveChanges();
         }
     }
 }
